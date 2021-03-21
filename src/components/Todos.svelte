b8f01c58329ef62e8ae3fc5c877e1d3e3eaaf8e7
sharmileeS <sharmilee.talekar@unoitservices.onmicrosoft.com>
2021-03-21 21:50:49 +0000

    add todo on enter

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -16,6 +16,11 @@
       todos = [...todos, { id: newTodoId, name: newTodoName, completed: false }]
       newTodoName = ''
     }
+    function onKeydown(event) {   
+    if (event.key === "Enter") {
+      addTodo()
+    }
+  }
   
     let filter = 'All'
     const filterTodos = (filter, todos) => 
@@ -23,7 +28,6 @@
       filter === 'Inactive' ? todos.filter(t => t.completed) : 
       todos
       let selected;
-   
   </script>
   
   <!-- Todos.svelte -->
@@ -39,11 +43,12 @@
                     What needs to be done?
                   </label>
                 </h2>
-                <div class="flex mt-4">
-                    <input bind:value={newTodoName} class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" placeholder="Add Todo">
-                    <button class="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal" on:click={() => addTodo()}>Add</button>
+                <div class="mt-4">
+                    <input bind:value={newTodoName} class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" placeholder="Add Todo(hit enter to add to list)" on:keydown={onKeydown}>
+                
                 </div>
             </div>
+  
   
   
   
