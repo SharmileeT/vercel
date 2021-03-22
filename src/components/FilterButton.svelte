8c3eb5fb4dfde50979110b0877a7227869cf9590
sharmileeS <sharmilee.talekar@unoitservices.onmicrosoft.com>
2021-03-22 11:38:10 +0000

    added  separate component for filter

---
+++ src/components/FilterButton.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let filter ='All'
+    let selected;
+</script>
+
+<div class="w-1/4">
+    <select bind:value={selected} on:change={()=> filter = selected}
+        class="m-1 h-full rounded border-t  border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
+        <option >All</option>
+        <option >Active</option>
+        <option >Inactive</option>
+    </select>
+
+  </div>
--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -1,5 +1,6 @@
 <!-- components/Todos.svelte -->
 <script>
+    import FilterButton from './FilterButton.svelte'
     export let todos = []
     
     let newTodoName = ''
@@ -52,14 +53,8 @@
   
   
   
-    <!-- Filter -->
-    <div class="w-1/4">
-      <select bind:value={selected} on:change={()=> filter = selected}
-          class="m-1 h-full rounded border-t  border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
-          <option >All</option>
-          <option >Active</option>
-          <option >Inactive</option>
-      </select>
+ <!-- Filter -->
+ <FilterButton bind:filter={filter}/> 
   
     </div>
     <!-- TodosStatus -->
